eba56df54151226615d6ebabd07b3e33898dd11b
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-11-07 23:00:03 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan59/detectiveconan/nama2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan59.detectiveconan;
+
+/**
+ *
+ * @author ACER
+ */
+public class nama2 extends karakter{
+    @Override
+     public void tampil(){
+        System.out.println("nama : "+nama);
+     }
+    @Override
+      public void ciri(){  
+        System.out.println("deskripsi : Ran Mouri teman Shinichi Kudo sejak kecil."
+                + " Ran dan Shinichi tidak pernah berani mengungkapkan perasaannya kepada Ran.");
+        System.out.println("");
+    }
+    
+}
